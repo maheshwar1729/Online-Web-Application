404fa463631180bf0010872bc2ee31c1bcacd571
maheshwar reddy <maheshwarreddy.c1729@gmail.com>
2019-03-14 14:21:33 +0100

    Update user.php

--- app/api/user.php
+++ app/api/user.php
@@ -1,40 +1,133 @@
-$app->get('/api/user', function(){
-          require_once ('connect.php');
-          $query = "SELECT * FROM ACCOUNTS order by id";
-          $result=$mysqli->query($query);
-          while($row=$result->fetch_assoc()) {
-          $data[] = $row;
-          }
-          if (isset($data)) {
-          echo json_encode($data);
-          }
-          
-          });
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
-$app->map (['GET','DELETE'],'/api/ACCOUNTS/{id}', function($request){
-           require_once ('connect.php');
-           $id = $request->getAttribute('id');
-           
-           $query1 = "DELETE from ACCOUNTS WHERE id = $id";
-           
-           $result1=$mysqli->query($query1);
-           });
+$app = new \Slim\App;
 
-$app->map (['GET','POST'],'/api/post/{EMAIL}/{PASSWORD}', function($request){
-           require_once ('connect.php');
-           $EMAIL= $request->getAttribute('EMAIL');
-           $PASSWORD = $request->getAttribute('PASSWORD');
-           $query2 = "INSERT INTO ACCOUNTS (EMAIL, PASSWORD) VALUES ($EMAIL,$PASSWORD)";
-           //$query = "INSERT INTO chat (user1,user2) VALUES ($user1,$user2)";
-           $result2=$mysqli->query($query2);
-           
-           });
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
+// Get All Customers
+$app->get('/api/get', function(Request $request, Response $response){
+    $sql = "SELECT * FROM ACCOUNTS";
+
+    try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $customers = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($customers);
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
+
+// Get Single Customer
+$app->get('/api/get/{id}', function(Request $request, Response $response){
+    $id = $request->getAttribute('id');
+
+    $sql = "SELECT * FROM ACCOUNTS WHERE id = $id";
+
+    try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $customer = $stmt->fetch(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($customer);
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
+
+// Add Customer
+$app->post('/api/post/add', function(Request $request, Response $response){
+    
+    	$EMAIL = $request->getParam('EMAIL');
+    	$PASSWORD = $request->getParam('PASSWORD');
+
+    $sql = "INSERT INTO ACCOUNTS (EMAIL, PASSWORD) VALUES ('EMAIL','PASSWORD')";
+
+    try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+
+        
+	$stmt->bindParam(':EMAIL', $EMAIL);
+	$stmt->bindParam(':PASSWORD', $PASSWORD);
+
+        $stmt->execute();
+
+        echo '{"notice": {"text": "Customer Added"}';
+
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
+
+// Update Customer
+$app->put('/api/update/{id}', function(Request $request, Response $response){
+    	//require_once ('connect.php');
+	$id = $request->getAttribute('id');
+    	$EMAIL = $request->getParam('EMAIL');
+    	$PASSWORD = $request->getParam('PASSWORD');
+    
+    	$sql = "UPDATE ACCOUNTS SET EMAIL = 'Z', PASSWORD = 'aa' WHERE id = $id";
+	try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+	$stmt->bindParam('Z', $EMAIL);
+	$stmt->bindParam('aa', $PASSWORD);
+	$stmt->execute();
+	echo '{"notice": {"text": "Customer Updated"}';
+
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
 
 
-$app->put('api/put/{PASSWORD}', function ($request) {
-          require_once ('connect.php');
-          $PASSWORD = $request->getAttribute('PASSWORD');
-          $query3 = "UPDATE ACCOUNTS SET PASSWORD = '1111' WHERE EMAIL = $PASSWORD";
-          $sth=$mysqli->query($query3);
-          
-          });
+// Delete Customer
+$app->delete('/api/delete/{id}', function(Request $request, Response $response){
+    $id = $request->getAttribute('id');
+
+    $sql = "DELETE FROM ACCOUNTS WHERE id = $id";
+
+    try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $db = null;
+        echo '{"notice": {"text": "Customer Deleted"}';
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
