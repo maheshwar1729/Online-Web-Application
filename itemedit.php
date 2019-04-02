6c1f57387b9a8ad50178d3b0f323d4f5feba3304
maheshwar reddy <maheshwarreddy.c1729@gmail.com>
2019-04-02 21:53:02 +0200

    Update itemedit.php

--- itemedit.php
+++ itemedit.php
@@ -1,8 +1,5 @@
 <?php
-
 include("db.php");
-
-
 if(isset($_POST['update']))
 {    
     $idl = $_POST['id'];   
@@ -11,9 +8,7 @@ if(isset($_POST['update']))
     $colour=$_POST['colour'];
     $size=$_POST['size'];
     $price=$_POST['price'];
-    $quantity=$_POST['quantity']; 
- 
-    
+    $quantity=$_POST['quantity'];    
     if(empty($type) || empty($brand) || empty($colour) || empty($size) ||  empty($price) || empty($quantity)) {            
         if(empty($type)) {
             echo "<font color='red'>Missing in type section</font><br/>";
@@ -32,12 +27,9 @@ if(isset($_POST['update']))
         } 
 	if(empty($quantity)) {
             echo "<font color='red'>Missing in quantity section</font><br/>";
-        
         }     
-    } else {    
-        
+    } else {
         $req = "UPDATE ITEMS SET type='$type',brand='$brand',colour='$colour',size='$size',price='$price',quantity='$quantity' WHERE ID=\"$idl\"";
-
 		if (mysqli_query($conn, $req)) 
 		  {
     		#echo "Item is updated successfully\n";
@@ -45,24 +37,16 @@ if(isset($_POST['update']))
 		else 
 		  {
     		echo "Error in updating Items " . mysqli_error($conn);
-		  }
-        
-        
+		  }      
         header("Location: totalitems.php");
     }
 }
 ?>
 <?php
-
 $ide = $_GET['id'];
-
-
-
 $req2 = $conn->query("SELECT * FROM ITEMS WHERE ID=\"$ide\"");
- 
 while($res = $req2->fetch_assoc())
-{
-    
+{ 
     $type = $res['type'];
     $brand = $res['brand'];
     $colour = $res['colour'];
@@ -71,7 +55,6 @@ while($res = $req2->fetch_assoc())
     $quantity = $res['quantity'];
 }
 ?>
-
 <html>
 <head>    
     <title>Clothing Store:Edit Data</title>
@@ -81,50 +64,41 @@ table {
     border-collapse: collapse;
     width: 40%;
 }
-
 td, th {
     border: 1px solid #dddddd;
     text-align: left;
     padding: 8px;
 }
-
 tr:nth-child(even) {
     background-color: #dddddd;
 }
 </style>
 </head>
- 
 <body>
     <a href="totalitems.php">List of total Items</a>
-    <br/><br/>
-    
+    <br/><br/>   
     <form name="form1" method="post" action="itemedit.php">
         <table border="1">
             <tr> 
                 <td>TYPE</td>
                 <td><input type="text" name="type" value="<?php echo $type;?>"></td>
             </tr>
-
             <tr> 
                 <td>BRAND</td>
                 <td><input type="text" name="brand" value="<?php echo $brand;?>"></td>
             </tr>
-
 	    <tr> 
                 <td>COLOUR</td>
                 <td><input type="text" name="colour" value="<?php echo $colour;?>"></td>
             </tr>
-
 	    <tr>
                 <td>SIZE(in SEK)</td>
                 <td><input type="text" name="size" value="<?php echo $size;?>"></td>
 	    </tr>
-
 	    <tr> 
                 <td>PRICE</td>
                 <td><input type="text" name="price" value="<?php echo $price;?>"></td>
             </tr>
-
 	    <tr> 
                 <td>QUANTITY</td>
                 <td><input type="text" name="quantity" value="<?php echo $quantity;?>"></td>
