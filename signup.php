26b187c33b22565f5465ee175b1144cf63d1022f
maheshwar reddy <maheshwarreddy.c1729@gmail.com>
2019-04-02 22:04:15 +0200

    Update signup.php

--- signup.php
+++ signup.php
@@ -4,15 +4,11 @@
 </head>
 <?php
 include("db.php");
- 
-
 if(isset($_POST['Submit'])) 
   {    
 	$username =$_POST['Username'];
     $password=$_POST['psw'];
     $confirmpassword = $_POST['psw-repeat'];
-	
-  
     echo "<br/><a href='home.html'>HOME page</a>";   
     if(empty($username) || empty($password)|| empty($confirmpassword)) 
 	  {
@@ -30,9 +26,6 @@ if(isset($_POST['Submit']))
             echo "<font color='red'>wrong Password: Re-enter Again</font><br/>";
           }
       }
-
-
-
     else 
 	  { 
 		$req = $conn->query("SELECT * FROM ACCOUNTS where EMAIL=\"$username\"");
@@ -40,39 +33,29 @@ if(isset($_POST['Submit']))
 		if($req->num_rows == 0) 
 		  {
 			if($password != $confirmpassword)
-			  {
-				
+			  {		
 				echo "<font color='red'>Passwords doesn't match</font><br/>";
 				echo "<br/><a href='signup.html'>Sign up again</a>"; 
 			  }
 			else
 			  {
-				
-
 				$sql = "INSERT INTO ACCOUNTS (EMAIL,PASSWORD) VALUES('$username','$password')";
 				if(mysqli_query($conn, $sql)){
 					#echo "Records inserted successfully.";
 				} else{
 					echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
 				}
-
 				echo "<br><font color='green'>USER Registered successfully.</font><br/>";
 				echo "<br><a href='login.html'>Login in</a>";
 			  }
 		  }
-
 		else
-		  {
-			
+		  {	
 			echo "<br><font color='red'>You have an account? Please Log-in</font><br/>";
 			echo "<br><a href='login.html'>Sign in</a></br>"; 
-		  }      
-       	
-        
-        
+		  }
     }
 }
-
 ?>
 </body>
 </html>
