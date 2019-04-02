ce69f2f75f72b8a5b9fa1e861febc6becfdd8c79
maheshwar reddy <maheshwarreddy.c1729@gmail.com>
2019-04-02 21:50:06 +0200

    Update deluser.php

--- deluser.php
+++ deluser.php
@@ -1,18 +1,11 @@
 <?php
 include("db.php");
- 
-
 $num = $_GET['id'];
-
- 
-
 $req =("DELETE FROM ACCOUNTS WHERE ID=$num");
-
 if (mysqli_query($conn, $req)) {
     #echo "Account is deleted successfully\n";
 } else {
     echo "Error deleting registered account " . mysqli_error($conn);
 }
- 
 header("Location:accounts.php");
 ?>
