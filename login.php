7b32062b8113e7168c90a7d6e495e0e4c23e744b
maheshwar reddy <maheshwarreddy.c1729@gmail.com>
2019-04-02 21:55:49 +0200

    Update login.php

--- login.php
+++ login.php
@@ -4,7 +4,6 @@
 </head>
 <?php
 include("db.php");
-
 # print "starting....";
 # print $_POST["submit"];
 if(isset($_POST["submit"])) 
@@ -12,33 +11,22 @@ if(isset($_POST["submit"]))
     # print "Inside....";
 	$box1 =$_POST['username'];  
 	$box2=$_POST['password'];
-
-    # print "$box1, $box2";
-         
+    # print "$box1, $box2";        
     if(empty($box1) || empty($box2)) {
 		echo "<br/><a href='login.html'>sorry to ask: Login again<br /></a>";
-
         if(empty($box1)) {
             echo "<font color='red'>Don't mess with username: empty username</font><br/>";
-        }
-        
-       
-        
-        
+        }     
 	if(empty($box2)) {
             echo "<font color='red'>Don't mess with passwords: empty password</font><br/>";
-        } 
-	
-       
-    } else { 
-        
+        }      
+    } else {   
        $req1 = $conn->query("SELECT * FROM ACCOUNTS");
 	$count =0;
 	while($row = $req1->fetch_assoc()) {
 		if($row['EMAIL']==$box1){
 			$count = $count+1;
 		}
-	
 	}
 	if($count==0){
 		echo "<font color='red'>There is no User with this credentials</font><br/>";
@@ -50,29 +38,21 @@ if(isset($_POST["submit"]))
 		while($row = $req2->fetch_assoc()) {
 			$pass = $row['PASSWORD'];
 			$id = $row['ID'];
-		
 		}
 		if ($pass ==$box2){
-			
 			echo "<font color='green'>Welcome to the store";
 			echo "<br/><a href=\"items.php?id=$id\">View Items in the store</a>";
 		}
 		else{
 			echo "<font color='red'>Please enter correct credentials</font><br/>";
 			echo "<br/><a href='login.html'>Sorry to ask: Login again</a>";
-			}
-	
-	        
-       	}
-        
-       
+			}   
+       	}      
     }
 }
-
 else {
    print "There is problem in your web page";
 }
-
 ?>
 <img src="image.jpg" width="100%" height="500">
 </body>
