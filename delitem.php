158053ee20598cfd238e8f3db26fb57d569c2831
maheshwar reddy <maheshwarreddy.c1729@gmail.com>
2019-04-02 21:49:29 +0200

    Update delitem.php

--- delitem.php
+++ delitem.php
@@ -1,19 +1,11 @@
 <?php
 include("db.php");
- 
-
 $id = $_GET['id'];
-
-
- 
-
 $req = "DELETE FROM ITEMS WHERE ID=$id";
-
 if (mysqli_query($conn, $req)) {
     #echo "Product is deleted successfully\n";
 } else {
     echo "Error in deleting item: " . mysqli_error($conn);
 }
- 
 header("Location:totalitems.php");
 ?>
