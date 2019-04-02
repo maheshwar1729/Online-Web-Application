72a0ba27aaa8545faca2a025901dc6428d5353f9
maheshwar reddy <maheshwarreddy.c1729@gmail.com>
2019-04-02 21:46:14 +0200

    Update delacc.php

--- delacc.php
+++ delacc.php
@@ -1,19 +1,12 @@
 <?php
 include("db.php");
- 
-
 $num = $_GET['id'];
 #print $num;
- 
-
 $req = "DELETE FROM ACCOUNTS WHERE ID=$num";
-
-
 if (mysqli_query($conn, $req)) {
     echo "Account Deleted successfully\n";
 } else {
     echo "Error in deleting Accont " . mysqli_error($conn);
 }
- 
 header("Location:home.html"); /* Redirect browser */
 ?>
